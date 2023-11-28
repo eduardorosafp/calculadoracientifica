f3b6d420c4872de991a0630d8f2c73daa33c8b29
Eduardo Flores <eduardoflores@MacBook-Air-de-Eduardo.local>
2023-11-28 16:23:29 -0300

    concertando para dispositivos moveis

--- src/Global/globlal-style.ts
+++ src/Global/globlal-style.ts
@@ -1,9 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-
-*{
+  * {
     margin: 0;
-    padding:0;
-}
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Arial', sans-serif;
+    background-color: #f4f4f4;
+    color: #333;
+  }
+
+  /* Adicione estilos específicos para telas menores (móveis) */
+  @media only screen and (max-width: 600px) {
+    /* Exemplo: Altere o tamanho da fonte ou a largura do conteúdo */
+    body {
+      font-size: 14px;
+    }
+
+    /* Adicione mais estilos conforme necessário para telas menores */
+  }
 `;
